46d3e0502c6ba93863b5aaf42bcf7722a84f2a3a
Pavankumardontha <pavankumardontha@gmail.com>
2022-12-18 18:50:38 +0530

    Update first_bad_version.cpp

--- first_bad_version.cpp
+++ first_bad_version.cpp
@@ -1,17 +1,25 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
 class Solution {
 public:
     int firstBadVersion(int n) 
     {
-        long left = 0;
+        int left = 1;
         int right = n;
-        while(left<right)
+        /* we want to find the index of the first bad version */
+        int first_index=-1;
+        while(left<=right)
         {
-            long mid = left + (right-left)/2;
+            int mid = left + (right-left)/2;
             if(isBadVersion(mid))
-            right = mid;
+            {
+                first_index = mid;
+                right = mid - 1;
+            }
             else
-            left = mid+1;
+            left = mid + 1;
         }
-        return left;
+        return first_index;
     }
 };
