b0e31058d909070273235388fe4e66a5b298e0c6
Pavankumardontha <pavankumardontha@gmail.com>
2023-01-05 15:48:14 +0530

    Update First_and_Last_element_with_given_property.cpp

--- First_and_Last_element_with_given_property.cpp
+++ First_and_Last_element_with_given_property.cpp
@@ -86,3 +86,52 @@ int last_element_with_propert()
 	}
 	return last_index;//returns -1 if no such elements exist.
 }
+
+
+int find_first_index(vector<int> & a, int target)
+{
+	// a = [1,2,2,2,2,4,5,5,5,6] -> if target = 2 this functions returns index 1 which is the first occurence index of target=2.
+    	int n = a.size();
+    	int left = 0;
+    	int right = n-1;
+    	int first_index = -1; 
+    	while(left<=right)
+    	{
+        	int mid = left + (right-left)/2;
+        	if(a[mid]<target)
+        	left = mid + 1;
+        	else 
+        	{
+            		/*we store the mid and reduce the search space by moving right index since we want the 
+            		first occurence index. We do the reverse ie move the left index if we want the last index
+            		occurence.*/
+            		if(a[mid]==target)
+            		first_index = mid;
+            		right = mid-1;
+        	}
+    	}
+    	return first_index;
+}
+
+int find_last_index(vector<int> &a, int target)
+{
+	// a = [1,2,2,2,2,4,5,5,5,6] -> if target = 2 this functions returns index 4 which is the last occurence index of target=2.
+    	int n = a.size();
+    	int left = 0;
+    	int right = n-1;
+    	int last_index = -1;
+    	while(left<=right)
+    	{
+        	int mid = left + (right-left)/2;
+        	if(a[mid]>target)
+        	right = mid-1;
+        	else
+        	{
+            		if(a[mid]==target)
+            		last_index = mid;
+            		left = mid+1;
+        	}
+    	}
+    	return last_index;
+}
+
