26a88fdb72d503a57f7135ec5550033ad785b893
Pavankumardontha <pavankumardontha@gmail.com>
2023-01-05 15:12:31 +0530

    Update First_and_Last_element_with_given_property.cpp

--- First_and_Last_element_with_given_property.cpp
+++ First_and_Last_element_with_given_property.cpp
@@ -5,6 +5,8 @@ bool property(int x)
 }
 
 /*
+
+CASE - 1:
 Before moving forward let us decribe the problem. Lets say we have a property which looks like 
 F,F,F,T,T,T,T applied on the array elements. 
 0,1,2,3,4,5,6
@@ -13,15 +15,23 @@ In the above case , we want to find the first index which is True and here the a
 the last false index which is one less than the first true index 
 last false index = first true index - 1
 
+CASE - 2:
 Similarly , in some of the cases, the property if applied on an array might look like this 
 T,T,T,F,F,F,F 
 0,1,2,3,4,5,6
 
 In the above case, we want to find the last index which is true and here the answer is index 2. By finding this index , we can also find the first false 
 index which is one more than the last true index. 
-
 first false index = last true index + 1
 
+CASE - 3:
+In this case we somewhat have a different scenario like 
+F F F F T T T F F F F F
+OR 
+T T T T F F F T T T T T
+
+Ex : find the first and last index of a target element in the sorted array [1,2,2,2,2,4,5,6,7,7,7]. 
+If the target element is 2 , the answer is [1,4]. So for these kind of problems also we use binary search. 
 */
 
 
