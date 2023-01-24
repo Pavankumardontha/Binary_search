20a800ce8b49c5b3ac2bcb870087dbbe388db108
Pavankumardontha <pavankumardontha@gmail.com>
2023-01-25 02:50:25 +0530

    Create Search_in_2D_matrix(leetcode).cpp

---
+++ Search_in_2D_matrix(leetcode).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+        // understand the difference between the gfg and leetcode problem
+        // start from the first row and last column (striver youtube approach)
+        int r = 0;
+        int c = matrix[0].size()-1;
+        while(r<matrix.size() and c>=0)
+        {
+            if(matrix[r][c]==target)
+            return true;
+            else if(target<matrix[r][c])
+            c--;
+            else
+            r++;
+        }
+        return false;
+    }
+};
