afb5219ffe463104d401d78e9472e1c39202f766
Pavankumardontha <pavankumardontha@gmail.com>
2023-01-26 02:01:10 +0530

    Update Find_peak_element.cpp

--- Find_peak_element.cpp
+++ Find_peak_element.cpp
@@ -11,6 +11,11 @@ d) if mid>0 and mid<n-1 then we will have both mid+1 and mid-1 elements to compa
 In each of the cases apart from a) we have a possibility of getting answer. So in each 
 step we need to check for answer
 
+What we are essentially doing is going in the direction of the rising slope(by choosing the element which is greater than current). How does that 
+guarantee the result? Think about it, there are 2 possibilities.a) rising slope has to keep rising till end of the array b) rising slope will encounter 
+a lesser element and go down.In both scenarios, we will have an answer. In 
+a) the answer is the end element because we take the boundary as -INFINITY 
+b) the answer is the largest element before the slope falls. 
 */
 
 int solve(vector<int> &a , int left, int right)
