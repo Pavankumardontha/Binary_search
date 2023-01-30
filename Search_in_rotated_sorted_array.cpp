028b0417e539c171da4b60babe5af27523514bd6
Pavankumardontha <pavankumardontha@gmail.com>
2023-01-31 01:30:07 +0530

    Update Search_in_rotated_sorted_array.cpp

--- Search_in_rotated_sorted_array.cpp
+++ Search_in_rotated_sorted_array.cpp
@@ -14,37 +14,46 @@ Mid index can either fall either in first sorted array or second sorted array.
   
 */
 
+int solve(vector<int>& a,int target)
+{
+    int left = 0;
+    int right = a.size()-1;
+    while(left<=right)
+    {
+        int mid = left + (right-left)/2;
+        if(a[mid]==target)
+        return mid;
+        else if(a[left]<=a[mid])
+        {
+            //mid falls in first sorted array
+            if(a[left]<=target and target<a[mid])
+            {
+                // target falls in [left,mid-1]
+                right = mid-1;
+            }
+            else
+            left = mid+1; // target falls in [mid+1,right]
+        }
+        else
+        {
+            // mid falls in second sorted array
+            if(a[mid]<target and target<=a[right])
+            {
+                // target falls in [mid+1,right]
+                left = mid+1;
+            }
+            else
+            right = mid-1; // target falls in [left,mid-1]
+
+        }
+    }
+    return -1;
+}
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) 
     {
-        int l = 0;
-        int r = nums.size()-1;
-        while(l<=r)
-        {
-            int mid = l + (r-l)/2;
-            if(target==nums[mid])
-            return mid;
-            
-            // if mid falls in left sorted portion
-            if(nums[l]<=nums[mid])
-            {
-                // we will use left and mid index as a basis to decide which side to go
-                if (target > nums[mid] or target < nums[l])
-                l = mid + 1;
-                else
-                r = mid - 1;
-            }
-            else
-            {
-                // if mid falls in right sorted array
-                // we use right and mid index as a basis to decide which side to go.
-                if( target < nums[mid] or target > nums[r])
-                r = mid - 1;
-                else
-                l = mid + 1;
-            }
-        }
-        return -1;
+        return solve(nums,target);
     }
 };
