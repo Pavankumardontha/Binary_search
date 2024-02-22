2329a7f192714dfd78e92d5755faa0552e9aee88
Pavan Kumar <pavankumardontha@gmail.com>
2024-02-22 18:52:44 +0530

    Update Search_in_rotated_sorted_array-2.cpp

--- Search_in_rotated_sorted_array-2.cpp
+++ Search_in_rotated_sorted_array-2.cpp
@@ -1,48 +1,51 @@
+/*
+Here there might be a problem where a[l] = a[r] = a[mid] in this case , we will not be able 
+to know in which direction to move either to the left or to the right. In this case , we will
+move to the left and as well to the right by 1, ie 
+l++ and r--;
+
+consider the case : [1,0,1,1,1],target=0 
+here l=0 and r=4 , mid=2 and a[mid]=a[l]=a[r]=1 here we will not know in which direction we need
+to move. We cannot exactly tell in which steep the target and mid lies.
+
+*/
+
 class Solution {
 public:
     bool search(vector<int>& a, int target) 
     {
-        int left = 0;
-        int right = a.size()-1;
-        while(left<=right)
+        int n = a.size();
+        int l = 0;
+        int r = n-1;
+        while(l<=r)
         {
-            int mid = left + (right-left)/2;
+            int mid = l + (r-l)/2;
             if(a[mid]==target)
             return true;
-            else if(a[left]==a[mid] and a[mid]==a[right])
+            if(a[l]==a[mid] and a[l]==a[r])
             {
-                // only different step from rotated sorted array 1
-                left++;
-                right--;
+                // extra case that needs to be handled than the previous problem
+                l++;
+                r--;
             }
-            else if(a[left]<=a[mid] and a[mid]<=a[right])
+            else if(a[l]<=target and a[l]<=a[mid])
             {
-                //[left,right] is sorted
-                if(a[left]<=target and target<a[mid])
-                right = mid-1;
+                if(a[mid]>target)
+                r=mid-1;
                 else
-                left = mid+1;
+                l=mid+1;
             }
-            else
+            else if(a[l]<=target and a[mid]<=a[r])
+            r=mid-1;
+            else if(a[l]<=a[mid] and target<=a[r])
+            l=mid+1;
+            else 
             {
-                //[left,right] is not sorted so either
-                //[left,mid] is sorted or 
-                // [mid,right] is sorted
-                if(a[left]<=a[mid] and a[mid]>=a[right])
-                {
-                    //[left,mid] is sorted
-                    if(a[left]<=target and target<a[mid])
-                    right = mid-1;
-                    else
-                    left = mid+1;
-                }
+                // if a[mid]<=a[r] and target<=a[r]
+                if(a[mid]>target)
+                r=mid-1;
                 else
-                {
-                    if(a[mid]<target and target<=a[right])
-                    left = mid+1;
-                    else
-                    right = mid-1;
-                }
+                l=mid+1;
             }
         }
         return false;
