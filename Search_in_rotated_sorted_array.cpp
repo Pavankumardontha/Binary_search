e270df6cef25d392f6ef5d815265b44d8401cb2c
Pavan Kumar <pavankumardontha@gmail.com>
2024-02-22 15:19:02 +0530

    Update Search_in_rotated_sorted_array.cpp

--- Search_in_rotated_sorted_array.cpp
+++ Search_in_rotated_sorted_array.cpp
@@ -1,54 +1,60 @@
 /*
-Initially , there are 2 sorted arrays. Left sorted array and the right sorted array.
-1) calculate mid and check if its equal to the target. If it is equal to target then return the index or 
-2) our [left,right] can either be sorted or not sorted.
-a) if it is sorted then we will apply simple binary search and move the left and right pointers accordingly.
-b) if [left,right] is not sorted then, [left,mid] or [mid,right] might be sorted. Either one of the arrays might be sorted and not both of them at the same
-time. Check if [left,mid] or [mid,right] is sorted and apply binary search only on the sorted part of the array.
+In a rotated sorted array with distinct elements always 
+nums[0] > nums[n-1]
+if its not , then it means its sorted
+Here in rotated sorted array we will have 2 steeps both increasing. The minimum of the first steep (first element in the rotated sorted array) is always greater than the maximum of the second
+steep(last element of the rotated sorted array)
+The elements present in the first steep are always greater than the elements in the second 
+steep since its rotated.
+There are 6 cases here 
+1) if a[mid] == target then return mid
+2) if the target falls in first steep(a[l]<=target) and mid also falls in first steep(a[l]<=a[mid])
+then we will have 2 conditions
+a) if a[mid]>target => r=mid-1
+b) if a[mid]<target => l=mid+1
+3) if the target falls in first steep(a[l]<=target) and mid falls in the second steep(a[mid]<=a[r])
+then r=mid-1
+4) if the target falls in second steep(target<=a[r]) and mid falls in first steep(a[l]<=a[mid])
+then l=mid+1
+5) if the target falls second steep(target<=a[r]) and mid is also in second steep(a[mid]<=a[r])
+then we will have 2 conditions
+a) if a[mid]<target => l=mid+1
+b) if a[mid]>target => r=mid-1
 */
 class Solution {
 public:
     int search(vector<int>& a, int target) 
     {
-        int left = 0;
-        int right = a.size()-1;
-        while(left<=right)
+        int n = a.size();
+        int l = 0;
+        int r = n-1;
+        int index=-1;
+        while(l<=r)
         {
-            int mid = left + (right-left)/2;
+            int mid = l + (r-l)/2;
             if(a[mid]==target)
             return mid;
-            else if(a[left]<=a[mid] and a[mid]<=a[right])
+            else if(a[l]<=target and a[l]<=a[mid])
             {
-                //[left,right] is sorted
-                if(target<a[mid])
-                right = mid-1;
+                if(a[mid]>target)
+                r=mid-1;
                 else
-                left = mid + 1;
+                l=mid+1;
             }
+            else if(a[l]<=target and a[mid]<=a[r])
+            r=mid-1;
+            else if(a[l]<=a[mid] and target<=a[r])
+            l=mid+1;
             else
             {
-                /*
-                [left,right] is not sorted. So either [left,mid] might be sorted or [mid,right] might be sorted
-                In other words , mid can fall either in the right sorted array or left sorted array*/
-                if(a[left]<=a[mid] and a[mid]>=a[right])
-                {
-                    // [left,mid] is sorted
-                    if(a[left]<=target and target<a[mid])
-                    right=mid-1;
-                    else
-                    left = mid+1;
-                }
+                // both target and mid are in second steep
+                // condition : (a[mid]<=a[r] and target<=a[r])
+                if(a[mid]>target)
+                r=mid-1;
                 else
-                {
-                    //[mid,right] is sorted
-                    if(a[mid]<=target and target<=a[right])
-                    left = mid+1;
-                    else
-                    right=mid-1;
-                    
-                }
+                l=mid+1;
             }
         }
-        return -1;
+        return index;
     }
 };
