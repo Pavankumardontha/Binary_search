4d424e5653325737e5605f9e36aea70e7404165c
Pavan Kumar <pavankumardontha@gmail.com>
2024-02-23 20:08:41 +0530

    Update Find_peak_element.cpp

--- Find_peak_element.cpp
+++ Find_peak_element.cpp
@@ -18,58 +18,42 @@ a) the answer is the end element because we take the boundary as -INFINITY
 b) the answer is the largest element before the slope falls. 
 */
 
-int solve(vector<int> &a , int left, int right)
-{
-    if(a.size()==1)
-    {
-        /*
-        Note that if we have only one element in our array, then mid==0 and also 
-        mid = a.size()-1 at the same time. There will not be any mid-1 element and 
-        mid+1 element to access. Care should be taken of this edge case.
-        */
-        return 0;
-    }
-    while(left<=right)
-    {
-        int mid = left + (right-left)/2;
-        if(mid>0 and mid<a.size()-1)
-        {
-            // both mid-1 and mid+1 indices will exist
-            if(a[mid-1]<a[mid] and a[mid]>a[mid+1])
-            return mid;
-            else if(a[mid-1]>a[mid])
-            right = mid-1;
-            else
-            left = mid+1;
-        }
-        else if(mid==0)
-        {
-            if(a[mid]>a[mid+1])
-            return mid;
-            else
-            {
-                //a[mid] <= a[mid+1]
-                left = mid+1;
-            }
-        }
-        else 
-        {
-            if(a[mid]>a[mid-1])
-            return mid;
-            else
-            {
-                //a[mid]<a[mid-1]
-                right = mid-1;
-            }
-        }
-    }
-    return -1;
-}
-
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) 
+    int findPeakElement(vector<int>& a) 
     {
-        return solve(nums,0,nums.size()-1);
+        int n = a.size();
+        int l=0;
+        int r=n-1;
+        if(n==1)
+        return 0;
+        while(l<=r)
+        {
+            int mid =  l + (r-l)/2;
+            if(mid==0)
+            {
+                if(a[mid]>a[mid+1])
+                return mid;
+                else
+                l=mid+1;
+            }
+            else if(mid==n-1)
+            {
+                if(a[mid]>a[mid-1])
+                return mid;
+                else
+                r=mid-1;
+            }
+            else
+            {
+                if(a[mid]>a[mid-1] and a[mid]>a[mid+1])
+                return mid;
+                else if(a[mid]<a[mid+1])
+                l=mid+1;
+                else
+                r=mid-1;
+            }
+        }
+        return -1;
     }
 };
