1bd81c04560c26105b184edd217fe0250d6d3800
Pavan Kumar <pavankumardontha@gmail.com>
2024-02-23 18:00:58 +0530

    Update Search_in_rotated_sorted_array-2.cpp

--- Search_in_rotated_sorted_array-2.cpp
+++ Search_in_rotated_sorted_array-2.cpp
@@ -1,19 +1,9 @@
 /*
 Here there might be a problem where a[l] = a[r] = a[mid] in this case , we will not be able 
-to know in which direction to move either to the left or to the right. In this case , we will
-move to the left and as well to the right by 1, ie 
-l++ and r--;
-
-consider the first case : [1,0,1,1,1],target=0 
-here l=0 and r=4 , mid=2 and a[mid]=a[l]=a[r]=1 here we will not know in which direction we need
-to move. We cannot exactly tell in which steep the target and mid lies.
-
-consider the second case : [1,2,0,1,1] , target=2
-l=0,r=4=> mid=2 a[l]=a[r]=1 a[mid]=0
-Here we can clearly decide on which direction to move. So only when a[l]=a[mid]=a[r] we cannot 
-decide the direction to move.
+to know whether [l,r] ,[l,mid] and [mid,r] is sorted or not. So we will not know in which direction to move either to the left or to the right. 
+In this case , we will move to the left and as well to the right by 1, ie l++ and r--;
+Apart from this , everything is same as the previous case with distinct elements.
 */
-
 class Solution {
 public:
     bool search(vector<int>& a, int target) 
@@ -26,30 +16,42 @@ public:
             int mid = l + (r-l)/2;
             if(a[mid]==target)
             return true;
-            if(a[l]==a[mid] and a[l]==a[r])
+            else if(a[l]==a[r] and a[l]==a[mid])
             {
-                // extra case that needs to be handled than the previous problem
                 l++;
                 r--;
             }
-            else if(a[l]<=target and a[l]<=a[mid])
+            else if(a[l]<=a[mid] and a[mid]<=a[r]) // [l,r] is sorted
             {
-                if(a[mid]>target)
-                r=mid-1;
-                else
-                l=mid+1;
+                //target lies in [a[l],a[r]]
+                if(a[l]<=target and target<=a[r])
+                {
+                    if(a[mid]>target)
+                    r=mid-1;
+                    else
+                    l=mid+1;
+                }
+                else // target does not lie in [a[l],a[r]]
+                return false;
             }
-            else if(a[l]<=target and a[mid]<=a[r])
-            r=mid-1;
-            else if(a[l]<=a[mid] and target<=a[r])
-            l=mid+1;
-            else 
+            else
             {
-                // if a[mid]<=a[r] and target<=a[r]
-                if(a[mid]>target)
-                r=mid-1;
+                //[l,r] is not sorted -> this means either [l,mid] is sorted or [mid,r] is sorted
+                if(a[l]<=a[mid]) // [l,mid] is sorted
+                {
+                    if(a[l]<=target and target<a[mid]) // check if target belongs to the sorted part
+                    r=mid-1;
+                    else
+                    l=mid+1;
+                }
                 else
-                l=mid+1;
+                {
+                    //[mid,r] is sorted
+                    if(a[mid]<target and target<=a[r]) // check if target belongs to the sorted part
+                    l=mid+1;
+                    else
+                    r=mid-1;
+                }
             }
         }
         return false;
