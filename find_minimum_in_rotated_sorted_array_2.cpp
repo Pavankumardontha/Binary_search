2b989a7874a852e5d930574fb391872209980bf4
Pavan Kumar <pavankumardontha@gmail.com>
2025-04-24 02:06:00 +0530

    Update and rename Find_min_in_rotated_sorted_array_2.cpp to find_minimum_in_rotated_sorted_array_2.cpp

--- find_minimum_in_rotated_sorted_array_2.cpp
+++ find_minimum_in_rotated_sorted_array_2.cpp
@@ -42,3 +42,54 @@ public:
         return minimum;
     }
 };
+
+
+/* Approach-2 : Same like search in rotated matrix - 2 */
+class Solution {
+public:
+    bool check_if_sorted(vector<int> &nums, int l, int r)
+    {
+        if(nums[l]<=nums[r])
+        return true;
+        return false;
+    }
+    int findMin(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int l = 0;
+        int r = n-1;
+        int ans = INT_MAX;
+        while(l<=r)
+        {
+            int mid = l + (r-l)/2;
+            if(nums[l] == nums[mid] and nums[mid]==nums[r])
+            {
+                /* same like search in rotated matrix-2 only this condition comes up extra to eliminate duplicates */
+                ans = min(ans,nums[l]);
+                l++;
+                r--;
+            }
+            else if(check_if_sorted(nums,l,mid))
+            {
+                ans = min(ans,nums[l]);
+                l=mid+1;
+            }
+            else
+            {
+                ans = min(ans,nums[mid]);
+                r=mid-1;
+            }            
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
