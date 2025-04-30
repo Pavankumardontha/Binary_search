bb55f65833d25c8ab19b8a178be8e3d1aa1ec2f9
Pavan Kumar <pavankumardontha@gmail.com>
2025-04-30 22:02:15 +0530

    Update find_peak_element-2.cpp

--- find_peak_element-2.cpp
+++ find_peak_element-2.cpp
@@ -60,3 +60,94 @@ public:
         return result;
     }
 };
+
+
+
+/* Same code written more neatly */
+class Solution {
+public:
+    bool isValid(int i, int j, int n, int m)
+    {
+        if(i>=0 and i<n and j>=0 and j<m)
+        return true;
+        return false;
+    }
+    int get(vector<vector<int>> &mat, int i, int j, int n, int m)
+    {
+        if(isValid(i,j,n,m))
+        return mat[i][j];
+        else
+        return -1;
+    }
+    vector<int> findPeakGrid(vector<vector<int>>& mat) 
+    {
+        /*
+        Same like peak element - 1 , instead of left and right, here we will have top,down,right,left 
+        We move in the direction of max of (top,down,left,right) if current index is not maximum. 
+        If current_index is maximum we return it. 
+        
+        */
+        // we will apply binary search on the rows
+        int n = mat.size();
+        int m = mat[0].size();
+        int l = 0;
+        int r = n-1;
+        vector<int> ans;
+        while(l<=r)
+        {
+            int mid_row =  l + (r-l)/2; // middle row
+            int mid_row_max = INT_MIN; // maximum element of the middle row
+            int mid_row_max_col_ind; // col index of the maximum element in the mid row
+
+            // iterate through all the elements in this row to find the maximum
+            for(int i=0;i<m;i++)
+            {
+                if(mid_row_max < mat[mid_row][i])
+                {
+                    mid_row_max = mat[mid_row][i];
+                    mid_row_max_col_ind = i;
+                }
+            }
+
+            /* 
+            we want to decide whethere we need to move up(r = mid_row-1) or whether we need to move down(l=mid+1). We can decide this by finding the maximum element 
+            among (mid_row-1, mid_row, mid_row+1)
+            */
+            if(mat[mid_row][mid_row_max_col_ind] < get(mat,mid_row-1,mid_row_max_col_ind,n,m) or mat[mid_row][mid_row_max_col_ind] < get(mat,mid_row+1,mid_row_max_col_ind,n,m))
+            {
+                // if mid element is the greatest less than either one of the (mid_row-1) and (mid_row+1)
+               if(get(mat,mid_row-1,mid_row_max_col_ind,n,m) > get(mat,mid_row+1,mid_row_max_col_ind,n,m))
+               {
+                    // move up
+                    r = mid_row-1;
+               }
+               else
+               {
+                    // move down
+                    l = mid_row+1;
+               }
+            }
+            else
+            {
+                // obvious answer is mat[mid_row][mid_row_max_col_id]
+                ans.push_back(mid_row);
+                ans.push_back(mid_row_max_col_ind);
+                break;
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
