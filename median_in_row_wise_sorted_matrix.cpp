2bbfb9dd6fb0bdd3160368bebadfe7183e819e2c
Pavan Kumar <pavankumardontha@gmail.com>
2025-05-25 19:03:00 +0530

    Update median_in_row_wise_sorted_matrix.cpp

--- median_in_row_wise_sorted_matrix.cpp
+++ median_in_row_wise_sorted_matrix.cpp
@@ -1,67 +1,56 @@
-int no_of_elements_greater_than_or_equal(vector<vector<int>> &mat, int x)
+// User function template for C++
+int no_of_elements_less_than_or_equal(vector<vector<int>> mat,int k)
 {
-    /*
-    Function used to find the no. of elements in the matrix greater than or equal to x.
-    for a row , let the first occurance of element greater than or equal to x be at index i. then the 
-    no. of elements in the matrix greater than or equal to x = n-i where n is the column size
-    */
-    int r = mat.size();
-    int c = mat[0].size();
-    int n = r*c;
-    int cnt = 0;
-    for(int i=0;i<r;i++)
+    int n = mat.size();
+    int m = mat[0].size();
+    int ans = 0;
+    for(int i=0;i<n;i++)
     {
-        int l=0;
-        int r=c-1;
-        int ans = n;
+        // find the no. of elements which are less than or equal to k in current row
+        int l = 0;
+        int r = m-1;
         while(l<=r)
         {
             int mid = l + (r-l)/2;
-            if(mat[i][mid]<x)
-            l=mid+1;
-            else
+            if(mat[i][mid]<=k)
             {
-                ans=mid;
-                r=mid-1;
-            }
-        }
-        cnt = cnt + (n-ans);
-    }
-    return cnt;
-}
-
-int no_of_elements_less_than_or_equal(vector<vector<int>> &mat, int x)
-{
-    /*
-    Funtion used to find the no. of elements in the matrix less than or equal to x.
-    */
-    int r = mat.size();
-    int c = mat[0].size();
-    int cnt = 0;
-    for(int i=0;i<r;i++)
-    {
-        int l=0;
-        int r=c-1;
-        int ans = -1;
-        while(l<=r)
-        {
-            int mid = l + (r-l)/2;
-            if(mat[i][mid]<=x)
-            {
-                ans=mid;
-                l=mid+1;
+                ans = ans + (mid-l+1);
+                l = mid+1;
             }
             else
             r=mid-1;
         }
-        cnt = cnt + (ans+1); // if ans==-1 cnt will not increment
     }
-    return cnt;
+    return ans;
 }
 
-class Solution{   
-public:
-    int median(vector<vector<int>> &matrix, int R, int C)
+int no_of_elements_greater_than_or_equal(vector<vector<int>> mat,int k)
+{
+    int n = mat.size();
+    int m = mat[0].size();
+    int ans = 0;
+    for(int i=0;i<n;i++)
+    {
+        int l = 0;
+        int r = m-1;
+        while(l<=r)
+        {
+            int mid = l + (r-l)/2;
+            if(mat[i][mid]>=k)
+            {
+                ans = ans + (r-mid+1);
+                r=mid-1;
+            }
+            else
+            l=mid+1;
+        }
+    }
+    return ans;
+}
+
+class Solution {
+  public:
+    int median(vector<vector<int>> &mat) 
     {
         /*
         Since R,C are odd , the no. of elements = R*C = odd and so there is one element for the median calculation
@@ -93,30 +82,32 @@ public:
         clearly both are equal. But the no of elements greater than or equal to 30 are 6 and the no. of elements less than or equal to 30 are 5.
         Both the values are greater than or equal to (n+1)/2. here n is the total no. of elements = 3*3 = 9
         */ 
+        int n = mat.size();
+        int m = mat[0].size();
         int minimum = INT_MAX;
         int maximum = INT_MIN;
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        int n = rows*cols; // no. of elements in the matrix
-        for(int i=0;i<rows;i++)
+        // minimum belongs to the first col and maximum belongs to the last col
+        for(int i=0;i<n;i++)
         {
-            minimum = min(minimum,matrix[i][0]);
-            maximum = max(maximum,matrix[i][cols-1]);
+            minimum = min(minimum,mat[i][0]);
+            maximum = max(maximum,mat[i][m-1]);
         }
-        
         int l = minimum;
         int r = maximum;
+        int total_no_of_elements = n*m;
+        int mid;
         while(l<=r)
         {
-            int mid = l + (r-l)/2;
-            int no_of_elements_less_than_or_equal_to_mid = no_of_elements_less_than_or_equal(matrix, mid);
-            int no_of_elements_greater_than_or_equal_to_mid = no_of_elements_greater_than_or_equal(matrix, mid);
-            if(no_of_elements_less_than_or_equal_to_mid == no_of_elements_greater_than_or_equal_to_mid)
+            mid = l + (r-l)/2;
+            int l1 = no_of_elements_less_than_or_equal(mat,mid);
+            int l2 = no_of_elements_greater_than_or_equal(mat,mid);
+            if(l1>=(total_no_of_elements+1)/2 and l2>=(total_no_of_elements+1)/2)
             return mid;
-            else if(no_of_elements_less_than_or_equal_to_mid < (n+1)/2)
+            else if(l1<(total_no_of_elements+1)/2)
             l=mid+1;
             else
             r=mid-1;
         }
     }
 };
+
