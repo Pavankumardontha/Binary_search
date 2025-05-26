e8b84c64d3f0db89a4e42d2c5f727e092c855118
Pavan Kumar <pavankumardontha@gmail.com>
2025-05-26 14:43:29 +0530

    Update kth_element_of_2_sorted_arrays.cpp

--- kth_element_of_2_sorted_arrays.cpp
+++ kth_element_of_2_sorted_arrays.cpp
@@ -1,89 +1,75 @@
-class Solution{
-    public:
-    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+class Solution {
+  public:
+    int kthElement(vector<int>& a, vector<int>& b, int k) 
     {
-        int no_of_elements_to_be_picked = k;
-        int no_of_elements_picked_from_arr1=0;
-        int no_of_elements_picked_from_arr2=0;
-        if(m>n)
-        {
-            //we apply BS only on the smaller of the 2 arrays
-            return kthElement(arr2,arr1,m,n,k);
-        }
-        int l1,l2,r1,r2;
+        /*
+        NOTE: Helps in solving the median of 2 sorted arrays. (do median of 2 sorted arrays before doing this)
+        Lets say we want to find the first k elements from 2 sorted arrays a,b
+        If we select x elements from a, we should select x-k elements from b
+        How do we know whether the selection is correct or not ??
+        based on the condition : 
+        l1<r1 and l2<r1
+        We need to pick the bounds of array a and array b which form the first
+        k elements when sorted combinely.
+        */
+        int total_no_of_elements_to_be_picked = k; 
+        int n1 = a.size();
+        int n2 = b.size();
         int l=0;
-        int r=min(n,k); // max no. of elements that we can pick from arr1. We have to pick the minium of (n,k)
+        int r=min(total_no_of_elements_to_be_picked,n1);
+        int mid; // no_of_elements that we are picked from array a
+        int no_of_elements_to_be_picked_from_b;
+        /* 
+        here we are directly applying binary search on array a, but we can find the 
+        shorted of both and apply bs on the shorted array
+        */
+        int l1,l2,r1,r2;
         while(l<=r)
         {
-            int mid = l + (r-l)/2;
-            no_of_elements_picked_from_arr1 = mid; // always less than n
-            no_of_elements_picked_from_arr2 = k - mid;
-            if(no_of_elements_picked_from_arr2 > m)
+            mid = l + (r-l)/2;
+            no_of_elements_to_be_picked_from_b = total_no_of_elements_to_be_picked - mid;
+            if(no_of_elements_to_be_picked_from_b > n2)
             {
-                //pick more elements from arr1
+                // we need to pick more elements from a
                 l=mid+1;
             }
             else
             {
-                l1 = INT_MIN;
-                l2 = INT_MIN;
-                r1 = INT_MAX;
-                r2 = INT_MAX;
-                
-                // l1 , r1 calculation
-                if(no_of_elements_picked_from_arr1 == 0)
-                {
-                    // no l1 
-                    r1 = arr1[no_of_elements_picked_from_arr1];
-                }
-                else if(no_of_elements_picked_from_arr1 == n)
-                {
-                    // no r1
-                    l1 = arr1[n - 1];
-                }
+                // find l1,l2,r1,r2 and check if its the correct combination or not
+                if(mid==0)
+                l1=INT_MIN;
                 else
-                {
-                    // both exist
-                    l1 = arr1[no_of_elements_picked_from_arr1-1];
-                    r1 = arr1[no_of_elements_picked_from_arr1];
-                }
+                l1=a[mid-1];
                 
-                // l2,r2 calculation
-                
-                if(no_of_elements_picked_from_arr2 == 0)
-                {
-                    // no l2 
-                    r2 = arr2[no_of_elements_picked_from_arr2];
-                }
-                else if(no_of_elements_picked_from_arr2 == m)
-                {
-                    //no r2
-                    l2 = arr2[m-1];
-                }
+                if(mid==n1)
+                r1=INT_MAX;
                 else
-                {
-                    // both exist
-                    l2 = arr2[no_of_elements_picked_from_arr2-1];
-                    r2 = arr2[no_of_elements_picked_from_arr2];
-                }
+                r1=a[mid];
                 
-                // check validity 
+                if(no_of_elements_to_be_picked_from_b==0)
+                l2=INT_MIN;
+                else
+                l2=b[no_of_elements_to_be_picked_from_b-1];
+                
+                if(no_of_elements_to_be_picked_from_b==n2)
+                r2=INT_MAX;
+                else
+                r2=b[no_of_elements_to_be_picked_from_b];
+                
                 if(l1<=r2 and l2<=r1)
                 {
-                    // valid 
+                    // correct combination
                     return max(l1,l2);
                 }
                 else if(l1>r2)
                 {
-                    // we need to push l to the right side to we move the right pointer to mid-1
-                    r = mid-1;
+                    // pick less from a
+                    r=mid-1;
                 }
                 else
-                {
-                    // similar to above
-                    l = mid + 1;
-                }
+                l=mid+1;
             }
         }
+        return -1;
     }
 };
