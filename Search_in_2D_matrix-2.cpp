b7cee623ea0c6334a3385634599da132ef699dff
Pavan Kumar <pavankumardontha@gmail.com>
2025-05-29 16:47:29 +0530

    Update Search_in_2D_matrix-2.cpp

--- Search_in_2D_matrix-2.cpp
+++ Search_in_2D_matrix-2.cpp
@@ -1,17 +1,27 @@
-// same solution as search in 2d matrix one
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    bool searchMatrix(vector<vector<int>>& mat, int target) 
     {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int r = 0;
-        int c = m-1;
-        while(r<n and c>=0)
+        /*
+        Read the conditions properly before going into the solution. There are 2 main conditions for this problem
+        a) Integers in each row are sorted in ascending from left to right.
+        b) Integers in each column are sorted in ascending from top to bottom.
+
+        Here are the solutions for the problem.
+        1) brute force approach O(M*N)
+        2) Apply binary search on each row or each column. Time complexity: O(MlogN) or O(NlogM)
+        3) Start from the top right corner. Compare it with the target and then eliminate the row or col accordingly
+        O(M+N)
+        */
+        int rows = mat.size();
+        int cols = mat[0].size();
+        int r=0;
+        int c=cols-1;
+        while(r<rows and c>=0)
         {
-            if(matrix[r][c]==target)
+            if(mat[r][c]==target)
             return true;
-            else if(target<matrix[r][c])
+            else if(target<mat[r][c])
             c--;
             else
             r++;
